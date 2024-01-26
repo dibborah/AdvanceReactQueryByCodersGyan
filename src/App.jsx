e1da8f4048cf123c8f48ffe1ab48ae558b5c1202
dibborah <dibyajyotiborah469@gmail.com>
2024-01-26 17:58:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App;
+
+// Advance concepts of React Query
+// 1. Paginated Queries
+// 2. Optimistic updates
+// 3. Dependent Queries
+// 4. Parallel Queries
