9325633b7cf8443c436b24076267046c41c34e60
dibborah <dibyajyotiborah469@gmail.com>
2024-01-27 20:41:54 +0530

    parallel queries done

--- src/parallel.jsx
+++ src/parallel.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
-// import { useQuery, useQueries } from '@tanstack/react-query';
+import { useQueries, useQuery } from '@tanstack/react-query';
 
 const Parallel = () => {
-    const [userIds, setUserIds] = React.useState([1]);
+    const [userIds, setUserIds] = React.useState([1, 2]);
+
+    // userIds.forEach((id)=>{
+    //     const userQuery = useQuery({
+    //         queryKey:['user', id],// unique key representing the query and parameter passed in the query need to be there in queryKey as values
+    //         queryFn: async () => {
+    //             const data = await fetch(`https://dummyjson.com/users/${id}`)
+    //             .then(res=> res.json());
+    //             return data;            
+    //         }
+    //     });
+    // })
+
+    const userQueries = useQueries({
+        queries: userIds.map((id)=>{
+            return {
+                queryKey: ["user", id],
+                queryFn: async () => {
+                    const data = await fetch(`https://dummyjson.com/users/${id}`)
+                    .then(res => res.json());
+                    return data;
+                }
+            }
+        })
+    })
 
     return (
-        <div>
+        <div style={{textAlign:"center"}}>
             <button
                 onClick={() =>
                     setUserIds((prev) => {
                         return [...prev, Date.now()];
+                        // return [...prev, Math.round((Math.random() * 10))];
                     })
                 }>
                 Load more
