5805ca7caed4905586d8182724ffb0c4171ede8f
dibborah <dibyajyotiborah469@gmail.com>
2024-01-28 21:22:31 +0530

    completed Dependent Query and git status(Double negation) which converts values to pure boolean value

--- src/dependant.jsx
+++ src/dependant.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 
 const fetchPostById = async (postId) => {
@@ -14,14 +13,23 @@ const fetchCommentsByPostId = async (postId) => {
 };
 
 const Dependant = () => {
+    // Dependent query
     const { data: post, isLoading } = useQuery({
         queryKey: ['post'],
         queryFn: () => fetchPostById(2),
     });
 
-    const { data: comments } = useQuery({
+    const postId = post?.id;
+
+    // React query main useQuery usually parallel main data fetch karti hain// Par dependent query main dependent query ko rokh deti hain("pending" state kar deti hain)
+    // React query is smart . Jab tak post.id undefined hoti hain react query fetchCommentsByPostId query ko pending state main dal deti hain aur data fetch nhi karti
+    // Aur jab is Query ko post.id mil jaegi query call karne ke liye wo is Query ko call kar degi. Aur data v fetch ho jaega
+
+    const { data: comments , status } = useQuery({
         queryKey: ['comments', post?.id],
         queryFn: () => fetchCommentsByPostId(post.id),
+        enabled: !!postId,// The double negation(!!)converts a falsy value(undefined, null etc.) to be false
+        // ... converts truthy values (like numbers and string) to be true. It basically converts any value to a PURE BOOLEAN value not just an equivalent one
     });
 
     return (
@@ -29,7 +37,7 @@ const Dependant = () => {
             <h1 className="text-lg font-bold">Post:</h1>
             {isLoading ? <p>Loading the post</p> : <h2>{post?.title}</h2>}
             <br />
-            <h1 className="text-lg font-bold">Comments</h1>
+            <h1 className="text-lg font-bold">Comments: {status === 'pending' && "Pending"}</h1>
             <ul>
                 {comments?.map((comment) => (
                     <p key={comment.id}>{comment.body}</p>
